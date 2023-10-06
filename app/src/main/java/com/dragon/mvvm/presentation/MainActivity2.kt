7918814986eb128b5043d839e7bbb99faec9f430
Alex Work Pc <apereverzev47@gmail.com>
2023-10-06 13:25:19 +0300

    Добавил новое активити

---
+++ app/src/main/java/com/dragon/mvvm/presentation/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.dragon.mvvm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dragon.mvvm.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
